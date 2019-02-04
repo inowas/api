d915f742a3c06e2cd0e84562151c2e6d1a79b429
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-02-04 14:16:17 +0100

    Clear DB before running tests

---
+++ api/tests/Controller/MessageBoxTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MessageBoxTest extends WebTestCase
+{
+    /**
+     * @test
+     */
+    public function sendCommandWithoutTokenReturns401()
+    {
+        $client = static::createClient();
+
+        $client->request(
+            'POST',
+            '/api/messagebox',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode([])
+        );
+
+        $this->assertEquals(401, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * test
+     */
+    public function sendInvalidCommandByValidUser()
+    {
+        $command = [
+            'message_name' => 'testMessage'
+        ];
+
+        $response = $this->sendCommand('admin', 'admin_pw', $command);
+        $this->assertEquals(322, $response->getStatusCode());
+    }
+
+    /**
+     * @param $username
+     * @param $password
+     * @param $command
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function sendCommand($username, $password, $command)
+    {
+        $client = static::createClient();
+
+        $client->request(
+            'POST',
+            '/api/login_check',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode(["username" => $username, "password" => $password])
+        );
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $content = json_decode($client->getResponse()->getContent(), true);
+        $token = $content['token'];
+
+        $client->request(
+            'GET',
+            '/api/users',
+            [],
+            [],
+            ['HTTP_Authorization' => sprintf('Bearer %s',  $token)],
+            json_encode($command)
+        );
+
+        return $client->getResponse();
+    }
+}
